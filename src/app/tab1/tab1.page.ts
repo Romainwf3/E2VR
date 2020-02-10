acefe9bc7d95154d7986e9c427ba5e9eae02294b
Romainwf3 <romainwf3@gmail.com>
2020-02-10 20:10:47 +0100

    Message de validation

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { LoadingController } from '@ionic/angular';
 
 @Component({
   selector: 'app-tab1',
@@ -7,6 +9,22 @@ import { Component } from '@angular/core';
 })
 export class Tab1Page {
 
-  constructor() {}
-
+  constructor(public http: HttpClient,public loadingController: LoadingController) {
+    this.getUsersList()
+    
+  }
+  usersList:any[]=[]
+  async getUsersList()
+  {
+    const loading = await this.loadingController.create({
+      message: 'Hello world !',
+      duration: 2000
+    });
+    await loading.present();
+    
+    this.http.get('https://randomuser.me/api/?results=10').subscribe(data=>{
+      this.usersList=data["results"]
+      loading.dismiss()
+    })
+  }
 }
