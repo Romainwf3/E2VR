dce397d6dbd6aed6260e23e46c64c6507f28b9d4
Romainwf3 <romainwf3@gmail.com>
2020-02-11 16:10:25 +0100

    Last push

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+// Importation des composants pour ma requête à l'API et pour recharger ma page
 import { HttpClient } from '@angular/common/http';
 import { LoadingController } from '@ionic/angular';
 
@@ -14,14 +15,14 @@ export class Tab1Page {
     
   }
   usersList:any[]=[]
-  async getUsersList()
-  {
+  async getUsersList() {
+    // création d'un message lors du rechargement de la page
     const loading = await this.loadingController.create({
       message: 'Hello world !',
       duration: 2000
     });
     await loading.present();
-    
+    // je fais une requête pour qu'il ne m'affiche que 10 utilisateurs 
     this.http.get('https://randomuser.me/api/?results=10').subscribe(data=>{
       this.usersList=data["results"]
       loading.dismiss()
